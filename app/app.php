b41c77c17ef8f40b952360e336aa4bac269f797e
FisherKyle <kyle@kylefisher.com>
2016-09-20 21:42:59 -0400

    fix improper syntax, the hardest of which to spot was a span of code in which the presence of dollar characters preventeed anything else from working.

--- app/app.php
+++ app/app.php
@@ -1,36 +1,37 @@
 <?php
 
-    require_once(__DIR__ . "/../vendor/autoload.php");
-    require_once(__DIR__ . "/../src/Contact.php");
-    date_default_timezone_set("America/New_York");
+    require_once(__DIR__ . '/../vendor/autoload.php');
+    require_once(__DIR__ . '/../src/Contact.php');
+    date_default_timezone_set('America/New_York');
 
     session_start();
 
-      if (empty($_SESSION['list_of_contacts'])) {
+      if (empty($_SESSION['list_of_contacts'])){
         $_SESSION['list_of_contacts'] = array();
       }
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(),array('twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use ($app) {
+    $app->get('/', function() use ($app){
 
-          return $app["twig"]->render("contacts_home.html.twig", array("contacts" => Contact::getAll()));
+          return $app['twig']->render('contacts_home.html.twig', array('contacts'=> Contact::getAll()));
     });
 
 // -----: contacts : add :-----//
 
-$app->post('/contacts_add', function() use ($app) {
-    $contact = new Contact($_POST["contact_name"], $_POST["contact_phone"], $_POST["contact_email"]); 
+$app->post('/contacts_add', function() use($app){
+    $contact = new Contact($_POST['contact_name'],$_POST['contact_phone'],$_POST['contact_email']);
     $contact->save();
 
-    return $app["twig"]->render("contacts_add.html.twig", array("new_contact" => $contact));
+    return $app['twig']->render('contacts_add.html.twig', array('new_contact' => $contact));
 });
 
 // -----:: contacts : delete ::------------------------------------------------>
-$app->post("/contacts_delete", function() use ($app) {
+$app->post('/contacts_delete', function() use($app){
     Contact::deleteAll();
-    return $app["twig"]->render("contacts_delete.html.twig");
+
+    return $app['twig']->render('contacts_delete.html.twig');
 });
 
 return $app;
--- src/Contact.php
+++ src/Contact.php
@@ -9,56 +9,50 @@
 
 // -----:: Object : constructor ::--------------------------------------------->
 
-        function __construct($phone, $name, $email) {
+        function __construct($name, $phone, $email) {
 
-            $this->contact_phone = $phone;
             $this->contact_name = $name;
+            $this->contact_phone = $phone;
             $this->contact_email = $email;
 
         }
 
 // G E T // S E T //
 
-        function setContactName($name) {
+        function setContactName($name){
           $this->$contact_name = (string) $name;
         }
 
-        function setContactPhone($phone) {
+        function setContactPhone($phone){
           $this->$contact_phone = (string) $phone;
         }
 
-        function getContactPhone() {
-          return $this->$contact_phone;
-        }
-
-        function getContactName() {
-          return $this->$contact_name;
-        }
-
-        function getContactEmail() {
-          return $this->$contact_email;
-        }
-
-        function setContactEmail($email) {
+        function setContactEmail($email){
           $this->$contact_email = (string) $email;
         }
 
-        // function newContact(
-        //   $this->"$contact_email"
-        // )
-
-        function save() {
-            array_push($_SESSION["list_of_contacts"], $this);
+        function getContactName(){
+          return $this->contact_name;
         }
 
-        static function getAll()
-        {
-            return $_SESSION["list_of_contacts"];
+        function getContactPhone(){
+          return $this->contact_phone;
         }
 
-        static function deleteAll()
-        {
-            $_SESSION["list_of_contacts"] = array();
+        function getContactEmail(){
+          return $this->contact_email;
+        }
+
+        function save(){
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll(){
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll(){
+            $_SESSION['list_of_contacts'] = array();
         }
 
     }
